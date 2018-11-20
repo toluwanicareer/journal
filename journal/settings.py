b7aff52623a93671470382d19d10da9810b16b38
Toluwani <abiodun.toluwanii@gmail.com>
2018-11-20 11:22:34 +0100

    'dfdf'

--- journal/settings.py
+++ journal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%807k$mrw9rn063v9ebgd%n^*y1730afqdv=cx)md96w5jz7hw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'journals.projecttopics.org']
 
